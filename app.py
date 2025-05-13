9454fb4f23aa38ba322954a183a35e070e019bcf
Gaby <mozart-neko@hotmail.com>
2025-05-12 22:33:15 -0700

    feat: histograma con graficas completo

--- app.py
+++ app.py
@@ -3,19 +3,20 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+st.header('sprint7')
+hist_button = st.checkbox('Construir histograma') # crear un boton
         
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
+if hist_button: # al hacer clic en el boton
+    # escribir un mensaje
+    st.write('Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un histograma
     fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
+    
+    # mostrar un grafico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
   
-if st.button('Construir gráfico de dispersión'):
-    st.write('Dispersión de odómetro vs. precio')
+if st.checkbox('Construir grafico de dispersion'):
+    st.write('Dispersion de odometro vs. precio')
     fig = px.scatter(car_data, x='odometer', y='price')
     st.plotly_chart(fig, use_container_width=True)
